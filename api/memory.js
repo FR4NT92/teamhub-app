9f5891f661ace47a835f1f024a14725740f06ee9
FR4NT92 <frantruppa@gmail.com>
2026-02-07 13:50:42 -0300

    Update memory.js

--- api/memory.js
+++ api/memory.js
@@ -1,4 +1,3 @@
-// api/memory.js
 export default async function handler(req, res) {
   // Solo aceptamos POST
   if (req.method !== 'POST') return res.status(405).json({ error: 'Method Not Allowed' });
